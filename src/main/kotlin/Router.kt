75f8b17096ade6186cba449220741d9684308350
wkpatrick <admin@wkpatrick.me>
2017-09-29 20:37:54 -0400

    Nothing interesting here :|

--- src/main/kotlin/Router.kt
+++ src/main/kotlin/Router.kt
@@ -2,24 +2,25 @@ import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 import java.net.Inet4Address
 import java.net.ServerSocket
-import java.util.concurrent.Semaphore
-import kotlin.concurrent.thread
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.sync.Mutex
 
 
 class Router(val portNumber: Int)
 //Need to get some sort of statistics set up.
 {
-    init{
+    init {
         val localhost = Inet4Address.getLocalHost()
         val serverSocket = ServerSocket(portNumber)
-        val historySemaphore = Semaphore(1)
 
         //The router will store each packet it recieves, this way we can get the send/receive stats from it when we construct it for later.
-        val routerHistory: ArrayList<RouterPacket>
+        val routerHistory: ArrayList<RouterPacket> = ArrayList<RouterPacket>()
+        val historyMutex = Mutex()
 
-
-        while(true){
-            thread {
+        while (true) {
+            val job = launch(CommonPool)
+            {
                 //Accepts the socket and starts doing its work
                 val clientSocket = serverSocket.accept()
                 val inputStream = ObjectInputStream(clientSocket.getInputStream())
@@ -29,12 +30,16 @@ class Router(val portNumber: Int)
                 //TODO: Ensure this doesn't create junk data when there is nothing trying to connect, check the SeverSocket.accept() docs? (It should block until a connection is made)
                 val tempData: RouterPacket = inputStream.readObject() as RouterPacket
                 //Send the message
+                outputStream.writeObject(tempData)
                 //Record the message
-
-                historySemaphore.acquire()
-                historySemaphore.release()
+                historyMutex.lock()
+                try {
+                    routerHistory.add(tempData)
+                } finally {
+                    historyMutex.unlock()
+                }
             }
-            //Look through the queue and send any packets that need to be sent.
+            //job.join()
         }
     }
 }
